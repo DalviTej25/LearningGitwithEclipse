fdfde55cd9c621e823b5ee2e95831321c61a861b
DalviTej25 <tejshree-ramesh.dalvi@capgemini.com>
2022-05-10 15:06:41 +0530

    1st commited master

---
+++ src/com/cg/java/master/Master.java
@@ -0,0 +1,11 @@
+package com.cg.java.master;
+
+public class Master {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Master branch");
+	}
+
+}
