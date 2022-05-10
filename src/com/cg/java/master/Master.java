d54214d4c08d84388b332fb95d68e250555d27f8
DalviTej25 <104520650+DalviTej25@users.noreply.github.com>
2022-05-10 15:33:49 +0530

    Update Master.java

--- src/com/cg/java/master/Master.java
+++ src/com/cg/java/master/Master.java
@@ -5,7 +5,7 @@ public class Master {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("Master branch");
+		System.out.println("Master is here");
 	}
 
 }
